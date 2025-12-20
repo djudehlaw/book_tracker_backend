c51e2b8d32151f7b81cfc16ec49804e16caff900
djudehlaw <dobraya.aglaya@gmail.com>
2025-12-20 13:13:14 +0300

    little fixes

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-import express from "express";
 import path from "path";
 import { fileURLToPath } from "url";
 
